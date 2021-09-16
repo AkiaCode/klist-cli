c1b7074f31f2dafcd6c2ce54ad7631aec5dfe3ef
Akiacode <catry.me@gmail.com>
2021-09-16 09:52:40 +0900

    A

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"os"
+)
 
+func main() {
+	if os.Args[1] == "search" {
+		SearchBot()
+	} else if os.Args[1] == "get" {
+		GetBot()
+	} else {
+		println("Unknown command")
+	}
 }
